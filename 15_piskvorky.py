de4f5783f5d53621bb4692efbb27ffc6f807fe8b
9matt <76255461+9matt@users.noreply.github.com>
2025-01-15 09:19:31 +0100

    uprava zadania a nova funkcia

--- 15_piskvorky.py
+++ 15_piskvorky.py
@@ -1,7 +1,6 @@
 # naprogramuj piskvorky
-# alternativy postup je za pomoci textu (pozri. komenty)
-# neviem, ci je nepresnost na pravej strane iba mac issue, alebo je niekde chyba v kode
 # na maturite sa na jedneho a druheho hraca pouzivaju dve tlacidla mysi, no tento sposob vyuziva premennu a jedno tlacidlo
+# doplnena verzia, kde prave tlacidlo "maze". Tiez nie je na maturite.
 
 import tkinter
 cislo = 1 # striedanie jedneho a druheho hraca
@@ -10,19 +9,31 @@ def tah(sur):
     global cislo
     okno = int(canvas.winfo_reqheight() / 15)
     offset = okno / 7
-    x = sur.x // okno * okno # + (okno / 2)
-    y = sur.y // okno * okno # + (okno / 2)
+    x = sur.x // okno * okno
+    y = sur.y // okno * okno
 
     if cislo % 2 != 0:
         canvas.create_line(x + offset, y + offset, x + okno - offset, y + okno - offset, width = offset, fill = 'blue')
         canvas.create_line(x + offset, y + okno - offset, x + okno - offset, y + offset, width = offset, fill = 'blue')
-        # canvas.create_text(x, y, text = "X", fill = "blue", font = ("Arial-Bold", 60))
     else:
         canvas.create_oval(x + offset, y + offset, x + okno - offset, y + okno - offset, width = offset, fill = 'white', outline = 'red')
-        
-        # canvas.create_text(x, y, text = "O", fill = "red", font = ("Arial-Bold", 60))
     
     cislo += 1
+
+def vymaz(sur): # toto nie je na maturite
+    global cislo
+    okno = int(canvas.winfo_reqheight() / 15)
+    offset = okno / 7
+    x = sur.x // okno * okno # + (okno / 2)
+    y = sur.y // okno * okno # + (okno / 2)
+
+    for i in range(6): # najprimitivnejsie riesenie toho, aby sa odstranili zvysky
+        canvas.create_line(x + offset, y + offset, x + okno - offset, y + okno - offset, width = offset, fill = 'white')
+        canvas.create_line(x + offset, y + okno - offset, x + okno - offset, y + offset, width = offset, fill = 'white')
+        canvas.create_oval(x + offset, y + offset, x + okno - offset, y + okno - offset, width = offset, fill = 'white', outline = 'white')
+    
+    cislo -= 1  # tu moze byt problem, keby niekto vymaze prazdne policko. 
+                # Riesenim by bola nejaka tabulka, ktora by sa checkla, ci tam nieco je.
 
 rozmer = 900
 canvas = tkinter.Canvas(height = rozmer, width = rozmer, bg = 'white')
@@ -40,4 +51,5 @@ for i in range(15):
     canvas.create_line(0, 0 + posun, rozmer, 0 + posun, fill = "black", width = "2")
 
 canvas.bind("<Button-1>", tah)
+canvas.bind("<Button-2>", vymaz)
 canvas.mainloop()
